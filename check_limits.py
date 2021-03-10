98d5157ae972621e07b6f6bcb622386d30f1108a
Rahul Devadiga <Devadiga.Rahul@in.bosch.com>
2021-03-10 15:04:10 +0530

    Re-work

--- check_limits.py
+++ check_limits.py
@@ -1,56 +1,84 @@
 ranges ={
-  'temp':{'min':0,'max':45},#Temperature
+  'temp':{'min':1,'max':45},#Temperature
   'soc':{'min':20,'max':80},#state of Charge
   'charge_rate':{'min':0,'max':0.8},#state of Charge
   }
 test_report=[]
-def collect_abnormals(abnormals,attribute_name,attribute_value,attribute_range):
-  if attribute_value < attribute_range['min'] or attribute_value > attribute_range['max']:
-    report="Attribute:"+attribute_name +"Actual value:"+str(attribute_value)+"[Expected Range::Min:"+str(attribute_range['min'])+"::Max:"+str(attribute_range['max'])
-    abnormals.append(report)
+test_case_id=0
+
+def append_list(src_list,dst_list):
+    for x in src_list:
+      dst_list.append(x)  
+
+def min_range_test(value,range_min):
+  result=False #Test step pass->Normal behavior
+  test_step='\t\tTeststep::Passed ->Actual value:'+str(value)+' is greater than Min Range:'+str(range_min)
+  if value < range_min:
+    test_step='\t\tTeststep::Failed ->Actual value:'+str(value)+' is less than Min Range:'+str(range_min)
+    result=True #Test step failed->abnormal behavior
+  return test_step,result
+
+def max_range_test(value,range_max):
+  result=False #Test step pass->Normal behavior
+  test_step='\t\tTeststep::Passed ->Actual value:'+str(value)+' is less than Max Range:'+str(range_max)
+  if value > range_max:
+    test_step='\t\tTeststep::Failed ->Actual value:'+str(value)+' is greater than Max Range:'+str(range_max)
+    result=True #Test step failed->abnormal behavior
+  return test_step,result
+
+def collect_abnormals(abnormals,test_case_report,attribute_name,attribute_value,attribute_range):
+
+  result=False #Test attribute is within Min and Max range->Normal behavior
+
+  test_step_min,min_range_result=min_range_test(attribute_value,attribute_range['min'])#test for min range
+  test_step_max,max_range_result=max_range_test(attribute_value,attribute_range['max'])#test for max range
+  
+  if min_range_result or max_range_result:
+    result=True #Test attribute is not within Min and Max range->abnormal behavior
+    abnormals.append(attribute_name)
+  return [test_step_min,test_step_max],result
 
 
 def report_abnormals_attribute(attribute_report):
   abnormals=[]
+  test_case_report=[]
+
   for attribute in attribute_report:
-    collect_abnormals(abnormals,attribute,attribute_report[attribute],ranges[attribute])
-  return abnormals
+    report='\tTest Attribute['+attribute+']::->Passed'#Test attribute is within Min and Max range->Normal behavior
+    test_step,result=collect_abnormals(abnormals,test_case_report,attribute,attribute_report[attribute],ranges[attribute])
+    if result==True:
+      report='\tTest Attribute['+attribute+']::->Failed' #Test attribute is not within Min and Max range->abnormal behavior
+    test_case_report.append(report)
+    append_list(test_step,test_case_report)
+  return test_case_report,abnormals
 
 def test_abnormals_attribute(battery_report):
-  
-  '''for line in report_abnormals_attribute(battery_report):
-    print(line)
-  assert(len(report_abnormals_attribute(battery_report))==0)'''
-  test_report.append(report_abnormals_attribute(battery_report))
 
-'''def battery_is_ok(temperature, soc, charge_rate):
-  if temperature < 0 or temperature > 45:
-    print('Temperature is out of range!')
-    return False
-  elif soc < 20 or soc > 80:
-    print('State of Charge is out of range!')
-    return False
-  elif charge_rate > 0.8:
-    print('Charge rate is out of range!')
-    return False
-
-  return True'''
+  global test_case_id
+  test_case_id+=1
+  test_case='Test Case ID['+str(test_case_id)+"]->Passed"
+  test_step,abnormals=report_abnormals_attribute(battery_report)
+  if(len(abnormals)!=0):
+    test_case='Test Case ID::['+str(test_case_id)+"]->Failed"
+  test_report.append(test_case)
+  append_list(test_step,test_report)
+  #assert(len(abnormals)==0)
 
 
 if __name__ == '__main__':
-  '''assert(battery_is_ok(25, 70, 0.7) is True)
-  assert(battery_is_ok(50, 85, 0) is False)'''
   test_input_battery_report_1 ={
     'temp':25,
     'soc':70,
     'charge_rate':0.7
     }
   test_input_battery_report_2 ={
-    'temp':25,
+    'temp':0,
     'soc':85,
     'charge_rate':0.9
     }
   test_abnormals_attribute(test_input_battery_report_1)
   test_abnormals_attribute(test_input_battery_report_2)
+  test_abnormals_attribute(test_input_battery_report_1)
   
-  print(test_report)
+  for line in test_report:
+    print(line)
